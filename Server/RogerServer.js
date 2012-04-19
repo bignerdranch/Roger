9a7616f43dc36e7c4d172d97794655df35082b1b
Chris Stewart <Cstew415@gmail.com>
2012-04-18 23:16:31 -0400

    Initial commit of the server component.

--- Server/RogerServer.js
+++ Server/RogerServer.js
@@ -40,30 +40,6 @@ http.createServer(function(request, response){
 	} 
 	
 	response.end();
-    // var full_path = path.join(process.cwd(),my_path);  
-    //     path.exists(full_path,function(exists){  
-    //         if(!exists){  
-    //             response.writeHeader(404, {"Content-Type": "text/plain"});  
-    //             response.write("404 Not Found\n");  
-    //             response.end();  
-    //         }  
-    //         else{  
-    //             filesys.readFile(full_path, "binary", function(err, file) {  
-    //                  if(err) {  
-    //                      response.writeHeader(500, {"Content-Type": "text/plain"});  
-    //                      response.write(err + "\n");  
-    //                      response.end();    
-    //   
-    //                  }  
-    //                  else{  
-    //                     response.writeHeader(200);  
-    //                     response.write(file, "binary");  
-    //                     response.end();  
-    //                 }  
-    //   
-    //             });  
-    //         }  
-    //     });  
 }).listen(port);  
 sys.puts("Server Running on " + port);
 
