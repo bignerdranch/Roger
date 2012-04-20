9b6dab1c5a29c3701faad2f3f75ee33549056a3e
Chris Stewart <Cstew415@gmail.com>
2012-04-19 21:24:34 -0400

    Improved UI of the android client

--- Android/Roger/src/com/bignerdranch/franklin/roger/RogerActivity.java
+++ Android/Roger/src/com/bignerdranch/franklin/roger/RogerActivity.java
@@ -10,7 +10,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 public class RogerActivity extends Activity {
     public static final String TAG = "RogerActivity";
@@ -47,7 +46,7 @@ public class RogerActivity extends Activity {
 
         if (id == 0) {
         	Log.e(TAG, "ID is 0. Not inflating.");
-        	showError();
+        	ErrorManager.show(RogerActivity.this, container, "Error loading view");
         	return;
         }
         
@@ -86,10 +85,6 @@ public class RogerActivity extends Activity {
     protected void onStop() {
     	super.onStop();
     	manager.setDownloadListener(null);
-    }
-    
-    private void showError() {
-    	Toast.makeText(this, "Unable to load view", Toast.LENGTH_SHORT).show();
     }
     
     private void startService() {
